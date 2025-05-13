a6f795a51474e7502a9781b100056093fd24e47a
Nayan <42470897+dragonfist630@users.noreply.github.com>
2025-05-14 00:37:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LoggedUser from './pages/LoggedUser/LoggedUser';
 import AuthUser from './pages/authUser/AuthUser';
 import RegisterUser from './pages/RegisterUser/RegisterUser';
-import { loadModels } from './modelLoader/modelLoader';
-import Login from './pages/Login/Login';
+import { loadModels } from './modelLoader/modelLoader'; import Login from './pages/Login/Login'
 
 export default function App() {
   useEffect(() => {
